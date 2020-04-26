5df1e7eabf9ca1f9f284342fae5ab17a6a8e7f9c
Ayush <ayush.mishra@fujitsu.com>
2020-04-26 18:24:05 +0530

    Initial Commit

---
+++ UniversaliOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversaliOSApp
+//
+//  Created by Ayush Mishra on 26/04/20.
+//  Copyright © 2020 Ayush Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
